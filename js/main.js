a6c8e27f3606b65dea664177943e13fd58e129dc
Ayman Mostafa <aymanmostafa@Aymans-MacBook-Air.local>
2025-05-18 06:11:08 +0300

    feat: animating section numbers

--- js/main.js
+++ js/main.js
@@ -36,3 +36,24 @@ const scrollTop = e => {
 // Event Listeneres
 window.addEventListener('scroll', toggleButton);
 document.getElementById('backToTop').addEventListener('click', scrollTop);
+
+new PureCounter({
+  // Setting that can't' be overriden on pre-element
+  selector: '.purecounter', // HTML query selector for specific element
+
+  // Settings that can be overridden on a per-element basis, by `data-purecounter-*` attributes:
+  start: 0, // Starting number [uint]
+  end: 100, // End number [uint]
+  duration: 2, // The time in seconds for the animation to complete [seconds]
+  delay: 10, // The delay between each iteration (the default of 10 will produce 100 fps) [miliseconds]
+  once: true, // Counting at once or recount when the element in view [boolean]
+  pulse: false, // Repeat count for a certain time [boolean:false|seconds]
+  decimals: 0, // How many decimal places to show. [uint]
+  legacy: true, // If this is true it will use the scroll event listener on browsers
+  filesizing: false, // This will enable/disable File Size format [boolean]
+  currency: false, // This will enable/disable the Currency format. Use it to set the symbol too [boolean|char|string]
+  formater: 'us-US', // Number toLocaleString locale/format, by default, is "en-US" [string|boolean:false]
+  separator: false, // This will enable/disable comma separator for thousands. Use it to set the symbol too [boolean|char|string]
+});
+
+new PureCounter();
